32215cb9dab3931c7445827431d116846f123102
smuenzinger <sarah.muenzinger@gmail.com>
2014-06-20 21:58:45 -0400

    maxid?

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ OAuth.popup('instagram', function(error, result){
   	if(error){
   		alert("Something funky happened!"+error);
   	}
-	result.get("https://api.instagram.com/v1/tags/dogsinhats/media/recent?max_id=01%2F01%2F2010").done(function(data){
+	result.get("https://api.instagram.com/v1/tags/dogsinhats/media/recent").done(function(data){
 	obj_to_explore = data;
 	$.each(data.data, function(i, item) {
 		var pic = showPic(item);
