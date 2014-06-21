5208443d1268e11cbee49a9479215463a79b1722
smuenzinger <sarah.muenzinger@gmail.com>
2014-06-20 21:28:38 -0400

    javascript attempt

--- app.js
+++ app.js
@@ -1,10 +1,4 @@
-// Initialize with your OAuth.io app public key
-OAuth.initialize('z1jbL8uDBlOVkjU4Czw1Up51u5c');
-OAuth.popup('instagram', function(error, success){
-  // See the result below
-alert(error);
 
-});
 
 var showPic = function(pic){
 
@@ -18,19 +12,18 @@ var showPic = function(pic){
   return result;
  };
 
-//recieves tweets from Twitter API
-var getDogs= function(){
-   var result= $.ajax({
-    url: "https://api.instagram.com/v1/tags/dogsinhats/media/recent?max_id=01%2F01%2F2010",
-    dataType: "jsonp",
-    type: "Get"
-   });
-   $.each(result.items, function(i, item) {
-      var pic = showPic(item);
-      $('.dog').append(pic);
-    });
- };
+// Initialize with your OAuth.io app public key
+OAuth.initialize('z1jbL8uDBlOVkjU4Czw1Up51u5c');
+OAuth.popup('instagram', function(error, success){
+  // See the result below
+
+	result.get("https://api.instagram.com/v1/tags/dogsinhats/media/recent?max_id=01%2F01%2F2010").done(function(data){
+	$.each(result.items, function(i, item) {
+		var pic = showPic(item);
+      	$('.dog').append(pic);
+	});
+	
 
 
-
- getDogs();
+});
+});
